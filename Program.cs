bf27252b5f29e97ad6b6d4d8c882c41e5afd7203
WhatIsFun <mohammed110051@gmail.com>
2023-06-20 14:30:58 +0400

    Overloading

--- Program.cs
+++ Program.cs
@@ -4,15 +4,32 @@
     {
         static void Main(string[] args)
         {
+
             Animal myAnimal = new Animal();  // Create a Animal object
             Animal myCat = new Cat();  // Create a Cat object
             Animal myDog = new Dog();  // Create a Dog object
+            //Dog dog = new Animal();
 
             myAnimal.animalSound();
             myCat.animalSound();
             myDog.animalSound();
+
+            add(2, 3);
         }
 
+
+        static int add(int a, int b)
+        {
+            return a + b;
+        }
+        static int add(int a, int b, int c)
+        {
+            return (a + b + c);
+        }
+        static double add(double a, double b)
+        {
+            return a + b;
+        }
         class Animal  // Base class (parent) 
         {
             public virtual void animalSound()
@@ -36,6 +53,7 @@
                 Console.WriteLine("The dog says: bow wow");
             }
         }
+
 
     }
 }

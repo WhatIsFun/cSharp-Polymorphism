19b693ccbef700321d612446c054f744c60867da
WhatIsFun <mohammed110051@gmail.com>
2023-06-20 11:12:48 +0400

    Polymorphism Example

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace cSharp_Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();  // Create a Animal object
+            Animal myCat = new Cat();  // Create a Cat object
+            Animal myDog = new Dog();  // Create a Dog object
+
+            myAnimal.animalSound();
+            myCat.animalSound();
+            myDog.animalSound();
+        }
+
+        class Animal  // Base class (parent) 
+        {
+            public virtual void animalSound()
+            {
+                Console.WriteLine("The animal makes a sound");
+            }
+        }
+
+        class Cat : Animal  // Child class
+        {
+            public override void animalSound()
+            {
+                Console.WriteLine("The cat says: meow");
+            }
+        }
+
+        class Dog : Animal  // Child class
+        {
+            public override void animalSound()
+            {
+                Console.WriteLine("The dog says: bow wow");
+            }
+        }
+
+    }
+}
